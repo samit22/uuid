5d92bf38841c7f6731a929f30ddc25dfde0c5039
Norbinsh <wowshay@gmail.com>
2020-10-05 08:33:25 +0300

    add simple test case

---
+++ cmd/check_test.go
@@ -0,0 +1,25 @@
+package cmd
+
+import "testing"
+
+func TestCheck(t *testing.T) {
+
+	t.Run("should return true", func(t *testing.T) {
+		input := "c923d13a-cee4-4182-af57-438f8b032fb6" // valid UUID
+		got := Check(input)
+		expect := true
+		if got != expect {
+			t.Errorf("expected %s to be %t but got %t", input, expect, got)
+		}
+	})
+
+	t.Run("collection of any size", func(t *testing.T) {
+		input := "c93a-cee4-4182-af57-438f2fb6" // not a valid UUID
+		got := Check(input)
+		expect := false
+		if got != expect {
+			t.Errorf("expected %s to be %t but got %t", input, expect, got)
+		}
+	})
+
+}
