8c60088517e74647163c8b22d45ae637a0257238
Norbinsh <wowshay@gmail.com>
2020-10-05 08:33:13 +0300

    move logic to a separate function, and support checking multiple arguments

--- cmd/check.go
+++ cmd/check.go
@@ -10,17 +10,26 @@ import (
 // check represents a command validating whether the provided input is a valid UUID
 var check = &cobra.Command{
 	Use:   "check",
-	Short: "Checks whether the provided input is a valid UUID",
-	Long: `Checks whether the provided input is a valid UUID,
+	Short: "Checks whether each of the provided arguments is a valid UUID",
+	Long: `Checks whether each of the provided arguments is a valid UUID,
 while using regular expression pattern matching.`,
 	Args: cobra.MinimumNArgs(1),
 	Run: func(cmd *cobra.Command, args []string) {
-		uuidPattern := "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
-		re := regexp.MustCompile(uuidPattern)
-		if re.MatchString(args[0]) {
-			fmt.Println("valid")
-			return
+		for _, arg := range args {
+			if Check(arg) {
+				fmt.Printf("%s is a valid UUID\n", arg)
+			} else {
+				fmt.Printf("%s is not a valid UUID\n", arg)
+			}
 		}
-		fmt.Println("invalid")
 	},
+}
+
+func Check(uuid string) bool {
+	uuidPattern := "^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$"
+	re := regexp.MustCompile(uuidPattern)
+	if re.MatchString(uuid) {
+		return true
+	}
+	return false
 }
