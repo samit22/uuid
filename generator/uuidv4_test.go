c84b54eef79bf2c393b104af8417709de2cba068
Andreas Krennmair <ak@synflood.at>
2021-10-03 16:20:38 +0200

    Add own UUID v4 generator. (#14)

--- generator/uuidv4_test.go
+++ generator/uuidv4_test.go
@@ -9,7 +9,10 @@ func TestGenerateV4(t *testing.T) {
 	var uuidPattern = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
 	t.Run("Return uuid v4", func(t *testing.T) {
 		g := Generate{}
-		uuid := g.V4()
+		uuid, err := g.V4()
+		if err != nil {
+			t.Fatalf("UUIDv4 generator returned error: %v", err)
+		}
 		re := regexp.MustCompile(uuidPattern)
 		if !re.MatchString(uuid) {
 			t.Errorf("invalid uuid genereated")
